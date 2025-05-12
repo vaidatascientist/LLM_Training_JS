fc66a67fd78b65c2105d6fa95a588464c77fc423
vai <vaibhavgupta.ggwp@gmail.com>
2025-05-12 18:09:53 -0400

    final commit 1

---
+++ bash_scripts/run-step-1.sh
@@ -0,0 +1,16 @@
+MAX_NEW_DATA=1000000
+
+python ./selfcodealign/src/star_align/self_ossinstruct.py \
+ --seed_data_files "./data/data.parquet" \
+ --use_vllm_server True \
+ --instruct_mode "S->C" \
+ --max_new_data $MAX_NEW_DATA \
+ --tag concept_gen \
+ --temperature 0.7 \
+ --seed_code_start_index 4400 \
+ --model "/project/phan/codellama/StarCoder" \
+ --num_fewshots 8 \
+ --num_batched_requests 200 \
+ --num_sample_per_request 1 \
+ --async_micro_batch_size 10 \
+ --delay 0
---
+++ bash_scripts/run-step-2.sh
@@ -0,0 +1,16 @@
+MAX_NEW_DATA=1000000
+
+python ./selfcodealign/src/star_align/self_ossinstruct.py \
+ --seed_data_files "./data/s_to_c.jsonl" \
+ --use_vllm_server True \
+ --instruct_mode "C->I" \
+ --max_new_data $MAX_NEW_DATA \
+ --tag concept_gen \
+ --temperature 0.7 \
+ --seed_code_start_index 0 \
+ --model "/project/phan/codellama/StarCoder" \
+ --num_fewshots 8 \
+ --num_batched_requests 200 \
+ --num_sample_per_request 1 \
+ --async_micro_batch_size 10 \
+ --delay 0
